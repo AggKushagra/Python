1f7a5e41d85e429e7c2588916105c0a5bd87408e
Kushagra Aggarwal <kushagra.aggarwal13@gmail.com>
2020-12-08 13:47:32 +0530

    Added Basic Calculator

---
+++ Basic Calculator.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=input("Enter the operator")
+
+if c=='+':
+    print("The answer is",a+b)
+elif c=='-':
+    print("The answer is", a-b)
+elif c=='*':
+    print("The answer is", a*b)
+elif c == '/':
+    print("The answer is", a/b)
+else:
+    print("Wrong Operator")
+
+
+
+
+
+
+
